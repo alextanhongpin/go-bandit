d7fe320badcdd8d43f2fc9982a165bf79472e998
alextanhongpin <alextan@seekasia.com>
2017-10-22 15:11:59 +0800

    chore: update plot

--- plot.py
+++ plot.py
@@ -2,11 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# "index", "chosen_arm", "reward", "cumulative_rewards"}
 
 df = pd.read_csv("annealing_softmax.csv")
 
 df["ratio"] = [ v / (k + 1) for k,v in enumerate(df["cumulative_rewards"])]
+df1 = df.drop("cumulative_rewards", axis=1) # axis 1 means column, not row
+df2 = df1.drop("index", axis=1)
+df3 = df2.drop("reward", axis=1)
+df4 = df3.drop("chosen_arm", axis=1)
 
-df.plot()
+df4.plot()
 plt.show()
 
